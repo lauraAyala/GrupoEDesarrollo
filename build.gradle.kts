de23519796e1e73542a109974824c48e6a96b9df
lauraAyala <lau-ayala@hotmail.com>
2023-04-17 21:22:34 -0300

    subo spring andando

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-
-
 plugins {
 	war
 	id("org.springframework.boot") version "3.0.5"
@@ -9,30 +7,45 @@ plugins {
 	kotlin("jvm") version "1.7.22"
 	kotlin("plugin.spring") version "1.7.22"
 	kotlin("plugin.jpa") version "1.7.22"
-	id ("org.sonarqube") version "3.5.0.2730"
 }
 
 group = "ar.edu.unq.desapp.grupoE"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
+
+
 repositories {
 	mavenCentral()
+	maven {
+		url = uri("https://repo.spring.io/release")
+	}
+	maven {
+		url = uri("https://repository.jboss.org/maven2")
+	}
 }
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
+	//implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.boot:spring-boot-starter-parent:2.5.5")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
-	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	//providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+	//testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
-	//implementation("org.springdoc:springdoc-openapi-ui:1.6.4")
-	//implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.0")
+	implementation("org.springframework.boot:spring-boot-starter-data-rest")
+	implementation("org.apache.camel:camel-spring-boot-starter:2.17.0")
+	//implementation("org.hibernate:hibernate-core")
+	//implementation("mysql:mysql-connector-java")
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+		testImplementation("junit:junit:4.12")
+	}
+
 }
 
 tasks.withType<KotlinCompile> {
@@ -45,5 +58,3 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
-
-
